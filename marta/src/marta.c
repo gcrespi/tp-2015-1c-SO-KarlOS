f495cea3a4bbf760f1381f3d4d2ab2a31b3d45d6
gcrespi <crespi.gustavo@gmail.com>
2015-06-04 22:29:11 -0300

    "Cosas" en marta

--- marta/src/marta.c
+++ marta/src/marta.c
@@ -27,36 +27,34 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include "../../connectionlib/connectionlib.h"
+#include "../../kbitarray/kbitarray.h"
 
 //************************* Campos De Archivo Conf ************************
 typedef struct {
 	int puerto_listen;
 	char* ip_fs;
 	int puerto_fs;
-}t_conf_MaRTA;
+} t_conf_MaRTA;
 
 //************************* Estructura Por cada Job Conectado ************************
 typedef struct {
 	int sockfd;
 	pthread_t* thr;
 	struct sockaddr_in socketaddr_cli;
-}t_hilo_job;
-
+} t_hilo_job;
 
 typedef struct {
 	int id_job;
 	int combiner;
 	char* path_result;
-	t_list file_list;
 	char** paths_files;
-}t_info_job;
+} t_info_job;
 
 typedef struct {
 	int id_file;
-//	char* path_file;
-//	int cantBloques;
-	t_bitarray* bloques_mapeados;
-}t_info_file;
+	t_kbitarray* bloques_mapeados;
+	pthread_mutex_t mutex;
+} t_info_file;
 
 typedef struct {
 	int id_temp;
@@ -67,18 +65,45 @@ typedef struct {
 } t_temp;
 
 
+//************************* Estructura Por cada Map ************************
+typedef struct {
+
+
+} t_hilo_map;
+
 //************************* Estructura de Carga de los Nodos (GLOBAL) ************************
 typedef struct {
 	int id_nodo;
-	in_addr_t ip_nodo;
-	int puerto_nodo;
-	int cant_op_map_en_curso;
-	int cant_op_reduce_en_curso;
-}t_nodo_carga;
+	in_addr_t ip_nodo; //XXX ???
+	int puerto_nodo; //XXX ???
+	int cant_ops_en_curso;
+//	int cant_op_map_en_curso;
+//	int cant_op_reduce_en_curso;
+} t_nodo_carga;
 
 //************************ Prototipos ******************************
 void levantar_arch_conf_marta(t_conf_MaRTA* conf);
 void hilo_conex_job(t_hilo_job *job);
+void terminar_hilos(); //XXX
+int programa_terminado(); //XXX provisorio, en la entrega no se usa
+int increment_and_take_last_job_id();
+
+void free_info_job(t_info_job info_job);
+int receive_info_job(t_hilo_job* job, t_info_job* info_job);
+
+int solicitar_info_de_archivo(char *path_file, t_info_file* info_file);
+int get_info_files_from_FS(char **paths_files, t_list* file_list);
+void free_info_file(t_info_file* self);
+void mostrar_info_file(t_info_file* self);
+
+void hilo_conex_job(t_hilo_job *job);
+void esperar_finalizacion_hilo_conex_job(t_hilo_job* job);
+void liberar_job(t_hilo_job* job);
+
+void free_conf_marta(t_conf_MaRTA* conf);
+void init_var_globales();
+void end_var_globales();
+
 
 //************************ Variables Globales ******************************
 pthread_mutex_t mutex_cerrar_marta;
@@ -87,9 +112,8 @@ int cerrar_marta = 0;
 pthread_mutex_t mutex_ultimo_id;
 int ultimo_id = 0;
 
-sem_t conex_fs_ready;
+sem_t conex_fs_ready;//XXX mutex??
 int socket_fs;
-pthread_t thr_conex_FS;
 
 t_log* paranoid_log;
 
@@ -98,6 +122,7 @@ void levantar_arch_conf_marta(t_conf_MaRTA* conf) {
 
 	char** properties = string_split("PUERTO_LISTEN,IP_FS,PUERTO_FS", ",");
 	t_config* conf_arch = config_create("marta.cfg");
+//	t_config* conf_arch = config_create("/home/gustavo/git/tp-2015-1c-karlos/marta/Debug/marta.cfg");
 
 	if (has_all_properties(3, properties, conf_arch)) {
 		conf->puerto_listen = config_get_int_value(conf_arch, properties[0]);
@@ -144,18 +169,6 @@ int increment_and_take_last_job_id() {
 	return aux_last_job_id;
 }
 
-
-//---------------------------------------------------------------------------
-void crear_hilo_para_un_job(t_hilo_job* job) {
-
-	log_info(paranoid_log, "Creación de Hilo Job");
-	if (pthread_create(job->thr, NULL, (void *) hilo_conex_job, job) != 0) {
-		log_error(paranoid_log, "No se pudo crear Hilo Job");
-		exit(-1);
-	}
-}
-
-
 //---------------------------------------------------------------------------
 void free_info_job(t_info_job info_job) {
 	free(info_job.path_result);
@@ -163,72 +176,38 @@ void free_info_job(t_info_job info_job) {
 }
 
 //---------------------------------------------------------------------------
-int recibir_info_job(t_hilo_job* job, t_info_job* info_job) {
+int receive_info_job(t_hilo_job* job, t_info_job* info_job) {
 
 //	t_info_job info_job;
 	char* aux_paths_to_apply_files;
-	int result = 0;
+	int result = 1;
 
 	info_job->id_job = increment_and_take_last_job_id();
-	log_debug(paranoid_log,"Info De Job: ID: %i",info_job->id_job);
+	log_debug(paranoid_log, "Info De Job: ID: %i", info_job->id_job);
 
-
-	result = (result != -1) ? recibir(job->sockfd, &(info_job->combiner)) : result;
-	if(result != -1) {
-		log_debug(paranoid_log,"Info De Job Recibida: Combiner: %i",info_job->combiner);
+	result = (result > 0) ? recibir(job->sockfd, &(info_job->combiner)) : result;
+	if (result > 0) {
+		log_debug(paranoid_log, "Info De Job Recibida: Combiner: %i", info_job->combiner);
 	}
 
-	result = (result != -1) ? recibir_dinamic_buffer(job->sockfd, (void **) &(aux_paths_to_apply_files)) : result;
-	if(result != -1) {
-		log_debug(paranoid_log,"Info De Job Recibida: Archivos: %s",aux_paths_to_apply_files);
+	result = (result > 0) ? recibir_dinamic_buffer(job->sockfd, (void **) &(aux_paths_to_apply_files)) : result;
+	if (result > 0) {
+		log_debug(paranoid_log, "Info De Job Recibida: Archivos: %s", aux_paths_to_apply_files);
 	}
 
-	result = (result != -1) ? recibir_dinamic_buffer(job->sockfd, (void **) &(info_job->path_result)) : result;
-	if(result != -1) {
-		info_job->paths_files = string_split(aux_paths_to_apply_files,",");
-		log_debug(paranoid_log,"Info De Job Recibida: Resultado: %s",info_job->path_result);
+	result = (result > 0) ? recibir_dinamic_buffer(job->sockfd, (void **) &(info_job->path_result)) : result;
+	if (result > 0) {
+		info_job->paths_files = string_split(aux_paths_to_apply_files, ",");
+		log_debug(paranoid_log, "Info De Job Recibida: Resultado: %s", info_job->path_result);
 	} else {
-		info_job->paths_files = malloc(sizeof(int));
+		info_job->paths_files = malloc(sizeof(char *));
 		*(info_job->paths_files) = NULL;
 	}
-
 
 	free(aux_paths_to_apply_files);
 
 	return result;
 }
-
-
-//---------------------------------------------------------------------------
-t_bitarray* crear_bitarray_limpio(int cant_bits)
-{
-	t_bitarray* self;
-	char* bitarray;
-	int i;
-	int cant_chars= cant_bits / 8;
-	if(cant_bits % 8 != 0) {
-		cant_chars++;
-	}
-
-	bitarray = malloc(cant_chars*sizeof(char));
-
-	for(i=0; i<cant_chars; i++) {
-		bitarray[i]=0;
-	}
-
-	self = bitarray_create(bitarray,cant_bits);
-
-
-	return self;
-}
-
-
-//---------------------------------------------------------------------------
-void free_bitarray(t_bitarray* self) {
-	free(self->bitarray);
-	bitarray_destroy(self);
-}
-
 
 //---------------------------------------------------------------------------
 int solicitar_info_de_archivo(char *path_file, t_info_file* info_file) {
@@ -236,7 +215,7 @@ int solicitar_info_de_archivo(char *path_file, t_info_file* info_file) {
 	int cant_bloques;
 	int result = 1;
 
-	log_debug(paranoid_log,"Pidiendo info del file: %s",path_file);
+	log_debug(paranoid_log, "Pidiendo info del file: %s", path_file);
 
 	sem_wait(&conex_fs_ready);
 	result = (result > 0) ? enviar_protocolo(socket_fs, INFO_ARCHIVO) : result;
@@ -245,45 +224,116 @@ int solicitar_info_de_archivo(char *path_file, t_info_file* info_file) {
 	result = (result > 0) ? recibir(socket_fs, &cant_bloques) : result;
 
 	sem_post(&conex_fs_ready);
-	log_debug(paranoid_log,"Holaaaa",path_file);
 
-	if(result <= 0) {
-		log_error(paranoid_log,"No se Pudo tomar info del Archivo: %s",path_file);
+	if (result <= 0) {
+		log_error(paranoid_log, "No se Pudo tomar info del Archivo: %s", path_file);
 	} else {
-		info_file->bloques_mapeados = crear_bitarray_limpio(cant_bloques);
+		info_file->bloques_mapeados = kbitarray_create_and_clean_all(cant_bloques);
 	}
 
 	return result;
 }
 
 //---------------------------------------------------------------------------
-int solicitar_info_de_archivos_a_FS(char **paths_files,t_list* file_list) {
+int get_info_files_from_FS(char **paths_files, t_list* file_list) {
 
-	int i,result=0;
+	int i, result = 1;
 	t_info_file* info_file;
 
-	for(i=0; (paths_files[i]!=NULL) && (result != -1); i++) {
+	for (i = 0; (paths_files[i] != NULL) && (result > 0); i++) {
 		info_file = malloc(sizeof(t_info_file));
-		result = solicitar_info_de_archivo(paths_files[i],info_file);
+		result = solicitar_info_de_archivo(paths_files[i], info_file);
 
-		if(result > 0) {
-			list_add(file_list,info_file);
+		if (result > 0) {
+			pthread_mutex_init(&(info_file->mutex), NULL);
+			list_add(file_list, info_file);
 		} else {
 			free(info_file);
 		}
 
 	}
 
-
+	if (result > 0) {
+		log_debug(paranoid_log, "Info de archivos solicitados con exito");
+		list_iterate(file_list, (void *) mostrar_info_file);
+	}
 	return result;
 }
 
 //---------------------------------------------------------------------------
-void free_info_file(t_info_file* info_file) {
+void free_info_file(t_info_file* self) {
 
-	free_bitarray(info_file->bloques_mapeados);
-	free(info_file);
+	kbitarray_destroy(self->bloques_mapeados);
+	pthread_mutex_destroy(&(self->mutex));
+	free(self);
 
+}
+
+//---------------------------------------------------------------------------
+void free_temp(t_temp* self) {
+
+	free(self);
+
+}
+
+//---------------------------------------------------------------------------
+void mostrar_info_file(t_info_file* self) {
+
+	int cant = kbitarray_get_size_in_bits(self->bloques_mapeados);
+	int mapeados = kbitarray_amount_bits_set(self->bloques_mapeados);
+
+	log_debug(paranoid_log, "ID Archivo: %i Cantidad Bloques: %i Mapeados: %i", self->id_file, cant, mapeados);
+}
+
+//---------------------------------------------------------------------------
+void free_hilos_map(t_temp* self) {
+
+	free(self);
+
+}
+
+
+//---------------------------------------------------------------------------
+int plan_maps(t_info_job info_job,t_list* file_list, t_list* temp_list) {
+
+	t_list* hilos_map = list_create();
+	int result = 1;
+
+	off_t block_without_map;
+	int file_without_map;
+
+	int _find_block_without_map_and_lock_it(t_info_file* info_file) {
+
+		pthread_mutex_lock(&(info_file->mutex));
+		block_without_map = kbitarray_find_first_clean(info_file->bloques_mapeados);
+		kbitarray_set_bit(info_file->bloques_mapeados,block_without_map);
+		file_without_map = info_file->id_file;
+		pthread_mutex_unlock(&(info_file->mutex));
+		return (block_without_map != -1);
+
+	}
+
+	while((list_find(file_list,(void *)_find_block_without_map_and_lock_it) != NULL) && (result > 0)) {
+//		log_info(paranoid_log,"Planificando map del Archivo:%i Bloque: %i ",file_without_map,block_without_map);
+	}
+
+	//TODO por acá dejé, hay que evaluar que tan conveniente sería tener hilos por cada rutina de map planificada
+
+
+	list_destroy_and_destroy_elements(hilos_map,(void *)free_hilos_map);
+	return 1;
+}
+
+
+//---------------------------------------------------------------------------
+int plan_reduces() {
+	return 1;
+}
+
+
+//---------------------------------------------------------------------------
+int save_result_file_in_MDFS() {
+	return 1;
 }
 
 //---------------------------------------------------------------------------
@@ -291,10 +341,10 @@ void hilo_conex_job(t_hilo_job *job) {
 	char *ip_job;
 	int port_job;
 	t_info_job info_job;
-	t_list* file_list;
+	t_list* file_list = list_create();
+	t_list* temp_list = list_create();
 
-
-	int result_OK = 0;
+	int result = 0;
 
 	getFromSocketAddrStd(job->socketaddr_cli, &ip_job, &port_job);
 
@@ -305,23 +355,19 @@ void hilo_conex_job(t_hilo_job *job) {
 	switch (prot) {
 
 	case NUEVO_JOB:
-		log_info(paranoid_log,"Obteniendo New Job del Job IP: %s, Puerto: %i",ip_job,port_job);
-		if(recibir_info_job(job, &info_job)!= -1) {
-			file_list = list_create();
-			if(solicitar_info_de_archivos_a_FS(info_job.paths_files,file_list) != -1) {
-				log_debug(paranoid_log,"Info de archivos solicitados con exito");
-			} else {
-				break;
-			}
-			//solicitar_info_de_bloque_FS(unDeterminadoBloqueDeArchivo);
-			//solicitar_info_de_Nodo(); (si no la tengo ya)
-			//calcular_cual_es_proxima_tarea()
-			//si es un map buscar el bloque donde mejor se hace
-			//si es un reduce ...
+		log_info(paranoid_log, "Obteniendo New Job del Job IP: %s, Puerto: %i", ip_job, port_job);
+		result = 1;
+		result = (result > 0) ? receive_info_job(job, &info_job) : result;
+		result = (result > 0) ? get_info_files_from_FS(info_job.paths_files, file_list) : result;
+		result = (result > 0) ? plan_maps(info_job,file_list,temp_list) : result;
+		result = (result > 0) ? plan_reduces() : result;
+		result = (result > 0) ? save_result_file_in_MDFS() : result;
 
-			list_destroy_and_destroy_elements(file_list,(void *) free_info_file);
-			result_OK = 1;
-		}
+		//solicitar_info_de_bloque_FS(unDeterminadoBloqueDeArchivo);
+		//solicitar_info_de_Nodo(); (si no la tengo ya)
+		//calcular_cual_es_proxima_tarea()
+		//si es un map buscar el bloque donde mejor se hace
+		//si es un reduce ...
 		break;
 
 	case DISCONNECTED:
@@ -329,7 +375,7 @@ void hilo_conex_job(t_hilo_job *job) {
 		break;
 
 	case -1:
-		log_error(paranoid_log, "No se pudo recivir new Job del Job");
+		log_error(paranoid_log, "No se pudo recibir new Job del Job");
 		break;
 
 	default:
@@ -337,20 +383,18 @@ void hilo_conex_job(t_hilo_job *job) {
 		break;
 	}
 
-	if(result_OK) {
+	if (result > 0) {
 		enviar_protocolo(job->sockfd, FINISHED_JOB);
-		log_info(paranoid_log, "Finalizado con éxito Job IP: %s, Puerto: %i",ip_job,port_job);
+		log_info(paranoid_log, "Finalizado con éxito Job IP: %s, Puerto: %i", ip_job, port_job);
 	} else {
 		enviar_protocolo(job->sockfd, ABORTED_JOB);
-		log_error(paranoid_log,"No se pudo realizar el Job IP: %s, Puerto: %i",ip_job,port_job);
+		log_error(paranoid_log, "No se pudo realizar el Job IP: %s, Puerto: %i", ip_job, port_job);
 	}
 
-	sleep(10); //XXX
-
-
 	free(ip_job);
-
 	free_info_job(info_job);
+	list_destroy_and_destroy_elements(file_list, (void *) free_info_file);
+	list_destroy_and_destroy_elements(temp_list, (void *) free_temp);
 }
 
 //---------------------------------------------------------------------------
@@ -373,18 +417,16 @@ void free_conf_marta(t_conf_MaRTA* conf) {
 	free(conf->ip_fs);
 }
 
-
 //---------------------------------------------------------------------------
 void init_var_globales() {
 
 	pthread_mutex_init(&mutex_cerrar_marta, NULL);
 	pthread_mutex_init(&mutex_ultimo_id, NULL);
 
-	sem_init(&conex_fs_ready,0,1);
+	sem_init(&conex_fs_ready, 0, 1);
 
 	paranoid_log = log_create("./logMaRTA.log", "MaRTA", 1, LOG_LEVEL_TRACE);
 }
-
 
 //--------------------------------------------------------------------------
 void end_var_globales() {
@@ -397,45 +439,10 @@ void end_var_globales() {
 	log_destroy(paranoid_log);
 }
 
-
-////-----------------------------------------------------------------------------
-//void abrir_conexion_con_FS(t_conf_MaRTA* conf_marta) {
-//
-//	int socket = 0;
-//	int segundos_reconexion= 5;
-//	int reconexiones = 0;
-//	const int max_reconexiones = 10;
-//
-//	socket = solicitarConexionCon(conf_marta->ip_fs,conf_marta->puerto_fs);
-//
-//	while((socket<=0) && (reconexiones <= max_reconexiones))
-//	{
-//		log_error(paranoid_log,"Error No se pudo Conectar al File System, volviendo a intentar en %i segundos",segundos_reconexion);
-//		sleep(segundos_reconexion);
-//		segundos_reconexion+= 5;
-//		reconexiones++;
-//
-//		socket = solicitarConexionCon(conf_marta->ip_fs,conf_marta->puerto_fs);
-//	}
-//
-//	if(socket >0)
-//	{
-//		socket_fs = socket;
-//		log_info(paranoid_log,"Conexion con FS establecida IP: %s, PORT: %i",conf_marta->ip_fs,conf_marta->puerto_fs);
-//		sem_post(&conex_fs_ready);
-//	}
-//	else
-//	{
-//		log_error(paranoid_log,"Demasiadas Reconexiones Sistema Bloqueado");//XXX al parecer no es necesaria una reconexion
-//	}
-//}
-
-
 //###########################################################################
 int main(void) {
 
 	init_var_globales();
-
 
 	t_conf_MaRTA conf;
 	levantar_arch_conf_marta(&conf); //Levanta el archivo de configuracion "marta.cfg"
@@ -448,19 +455,12 @@ int main(void) {
 
 	lista_jobs = list_create();
 
-
-//	log_info(paranoid_log, "Creando Hilo Para Conectar FS");
-//	if (pthread_create(&thr_conex_FS, NULL, (void *) abrir_conexion_con_FS, &conf) != 0) {
-//		log_error(paranoid_log, "No se pudo crear Hilo Para Conectar FS");
-//		exit(-1);
-//	}
-
-	if((socket_fs = solicitarConexionCon(conf.ip_fs,conf.puerto_fs)) == -1) {
-		log_error(paranoid_log,"No se pudo Conectar al FS");
+	log_info(paranoid_log, "Conectando con FS...");
+	if ((socket_fs = solicitarConexionCon(conf.ip_fs, conf.puerto_fs)) == -1) {
+		log_error(paranoid_log, "No se pudo Conectar al FS");
 		exit(-1);
 	}
-
-
+	log_info(paranoid_log, "Conectado a FS");
 
 	log_debug(paranoid_log, "Obteniendo Puerto para Escuchar Jobs...");
 	if ((listener_jobs = escucharConexionesDesde("", conf.puerto_listen)) == -1) {
@@ -481,11 +481,15 @@ int main(void) {
 			exit(-1);
 		}
 
-		crear_hilo_para_un_job(job);
+		log_info(paranoid_log, "Creación de Hilo Job");
+		if (pthread_create(job->thr, NULL, (void *) hilo_conex_job, job) != 0) {
+			log_error(paranoid_log, "No se pudo crear Hilo Job");
+			exit(-1);
+		}
 
 		hilos_de_job++;
 
-		if (hilos_de_job >= 3) {//XXX
+		if (hilos_de_job >= 3) { //XXX
 			terminar_hilos();
 		}
 	}
